240938c6f17e22c63ede67d4fe116b7896c0fdb9
Rohit-code <53340758+Rohit-code@users.noreply.github.com>
2024-06-28 22:04:10 +0530

    styles

--- src/components/SummarySheet.js
+++ src/components/SummarySheet.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import '../styles/SummarySheet.css'; // Import the CSS file
 
 function SummarySheet() {
   const [summary, setSummary] = useState([]);
